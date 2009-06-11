72c252d7ec9dfb7a710c2e0e36c5a5e685416267
mike <>
2009-06-11 23:38:19 +0000

    Postgresql dialect

---
+++ source/spiralcraft/sql/vendor/postgresql/PostgresqlDialect.java
@@ -0,0 +1,33 @@
+//
+// Copyright (c) 2009 Michael Toth
+// Spiralcraft Inc., All Rights Reserved
+//
+// This package is part of the Spiralcraft project and is licensed under
+// a multiple-license framework.
+//
+// You may not use this file except in compliance with the terms found in the
+// SPIRALCRAFT-LICENSE.txt file at the top of this distribution, or available
+// at http://www.spiralcraft.org/licensing/SPIRALCRAFT-LICENSE.txt.
+//
+// Unless otherwise agreed to in writing, this software is distributed on an
+// "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+//
+package spiralcraft.sql.vendor.postgresql;
+
+import spiralcraft.sql.Dialect;
+
+public class PostgresqlDialect
+  extends Dialect
+{
+
+  /**
+   * The default schema name to use for application tables if no specific
+   *   schema name is specified.
+   * 
+   * @return
+   */
+  @Override
+  public String getDefaultSchemaName()
+  { return "public";
+  }
+}
